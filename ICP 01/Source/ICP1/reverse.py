96cbb2c85f394ec2ae75e9e4692aed7715ce7207
Fatema Hasta <fhasta@naic.org>
2018-08-24 16:53:16 -0500

    Pushed source code

---
+++ ICP 01/Source/ICP1/reverse.py
@@ -0,0 +1,10 @@
+first_name = input("\n  Enter first name \n")
+last_name = input("\n  Enter last name \n")
+
+concated_string = first_name + last_name
+print("The concatenated string is: ")
+print(concated_string)
+
+print("The reversed string is: ")
+reverse_string = concated_string[::-1]
+print(reverse_string)
