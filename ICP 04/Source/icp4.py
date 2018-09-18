c8d551ee320c1d6fe3dd60e0e057e14546c7c35c
vinayjaibheem <vinayjois12@gmail.com>
2018-09-18 00:07:40 -0500

    Lab Assignment

--- ICP 04/Source/icp4.py
+++ ICP 04/Source/icp4.py
@@ -3,5 +3,5 @@ import numpy as np
 
 vector_rand = np.random.randint(0,21,size=15)
 print("Vector with random integers between 0 and 20: ",vector_rand)
-#print(np.bincount(vector_rand))
+print(np.bincount(vector_rand))
 print("Most frequent value in the vector list: ",np.argmax(np.bincount(vector_rand)))
