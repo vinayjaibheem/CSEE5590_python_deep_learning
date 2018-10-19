0cf6d66d469c52093ac367e503fa1cd205869f09
Fatema Hasta <fatemahasta95@gmail.com>
2018-10-19 17:50:52 -0500

    Made changes

---
+++ LAB3/Source/LabAssignment3/LogisticRegression1.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:199978b6b8964e318f26d336421dc9c49b12357f3c56fe55cf39fabab148b010
+size 1243
---
+++ LAB3/Source/LabAssignment3/venv/bin/activate.csh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dbd70b8c6753c43873b5e7b4e6d0d413767f7b57a2053ac9c9070c9fbb220f99
+size 1280
